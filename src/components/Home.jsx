dfba44fccb4ef8fb79def195346749dccab6d324
lolaakinrinsola <ololadeakinrinsola@gmail.com>
2023-03-02 21:55:48 +0100

    html

---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <div>
+            <h1>LITTLE LEMON</h1>
+            <h2>CHICAGO</h2>
+            <p>Little lemon is a charming neighborhood bistro
+                that serves simple food and classic cocktails in a lively
+                but casual environment. The restaurant features a locally-soured 
+                menu with daily specials
+            </p>
+            <button>RESERVATION</button>
+        </div>
+        <div>
+            <img src="" alt="" />
+        </div>
+      </header>
+
+      <body>
+        <div>
+            <h2>SPECIALS</h2>
+            <button>RESERVATION</button>
+        </div>
+        <div>
+
+        </div>
+      </body>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      <div>
+        <ul>
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>RESERVATIONS</li>
+            <li>MENU</li>
+            <li>ORDER ONLINE</li>
+            <li>LOGIN</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
